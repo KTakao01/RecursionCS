5f239e39873f8b6b67fe139d6d708be3e0dbb52f
KTakao01 <takoizmi22@gmail.com>
2023-08-03 22:22:13 +0900

    Initial commit

---
+++ UserProfileCards/getElementById.js
@@ -0,0 +1,19 @@
+<!--
+  問題文
+  以下のエディターにHTMLが書かれているので、特定のIDを持つ要素を取得し、変数に保存し、コンソール上に出力しましょう。
+-->
+<div>
+    <h1 id="title">Recursion</h1>
+    <div>
+        <p id="cs">Computer Science</p>
+        <p id="pr">Programming</p>
+    </div>
+</div>
+
+// ここからJavaScriptを記述してください。要素をコンソール上に出力しましょう。
+const ele1 = document.getElementById("title");
+console.log(ele1);
+const ele2 = document.getElementById("cs");
+console.log(ele2);
+const ele3 = document .getElementById("pr");
+console.log(ele3);
