c37eb82c0bc448a51d3503ff5dcac11b960cf167
KTakao01 <takoizmi22@gmail.com>
2023-08-10 21:55:27 +0900

    0810

---
+++ expertcourse/01TheClassOfSinglyLinkedList.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Item struct {
+	data int
+	next *Item
+}
+
+func NewItem(data int) *Item {
+	return &Item{data: data}
+}
+
+type SinglyLinkedList struct {
+	head *Item
+}
+
+func NewSinglyLinkedList(item *Item) *SinglyLinkedList {
+	return &SinglyLinkedList{head: item}
+}
+
+func main() {
+	item1 := NewItem(7)
+	item2 := NewItem(99)
+	item3 := NewItem(45)
+
+	numList := NewSinglyLinkedList(item1)
+
+	item1.next = item2
+	item2.next = item3
+
+	currentNode := numList.head
+	for currentNode != nil {
+		fmt.Println(currentNode.data)
+		currentNode = currentNode.next
+	}
+}
---
+++ expertcourse/linked list/02changeArrayFromLinkedList.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	data int
+	next *Node
+}
+
+func NewNode(data int) *Node {
+	return &Node{data: data}
+}
+
+type SinglyLinkedList struct {
+	head *Node
+}
+
+func NewSinglyLinkedList(data int) *SinglyLinkedList {
+	return &SinglyLinkedList{head: &Node{data: data}}
+}
+
+func main() {
+	array := []int{35, 23, 546, 67, 86, 234, 56, 767, 34, 1, 98, 78, 555}
+
+	//for i:=0,i<array.length(),i++{
+
+	//}
+
+	numList := NewSinglyLinkedList(array[0])
+	currentNode := numList.head
+
+	//リストに他のノードを追加します
+	for _, val := range array[1:] {
+		currentNode.next = NewNode(val)
+		currentNode = currentNode.next
+	}
+
+	currentNode = numList.head
+	//連結リストを反復して出力する
+	for currentNode != nil {
+		fmt.Println(currentNode.data)
+		currentNode = currentNode.next
+	}
+}
