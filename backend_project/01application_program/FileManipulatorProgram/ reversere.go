6ce1073cfce0e948ce830f653271e5798c03f1b5
KTakao01 <takoizmi22@gmail.com>
2023-09-10 12:11:55 +0900

    0909

---
+++ backend_project/01application_program/FileManipulatorProgram/ reversere.go
@@ -0,0 +1,59 @@
+分割するだけ
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run file_manipulator.go reverse <inputfile> <outputfile>")
+		return
+	}
+
+	command := os.Args[1]
+	inputFile := os.Args[2]
+	outputFile := os.Args[3]
+
+	switch command {
+	case "reverse":
+		err := reverse(inputFile, outputFile)
+		if err != nil {
+			fmt.Println("Error processing reverse:", err)
+		}
+	}
+}
+
+// reverse ファイル内容を反転して、指定された出力ファイルに保存します
+func reverse(inputFile, outputFile string) error {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		return fmt.Errorf("Error reading file: %w", err)
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("Error reading content: %w", err)
+	}
+
+	reversedData := reverseString(string(content))
+
+	err = os.WriteFile(outputFile, []byte(reversedData), 0644)
+	if err != nil {
+		return fmt.Errorf("Error writing to file: %w", err)
+	}
+	return nil
+}
+
+// reverseString は文字列を逆にして返します
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
--- backend_project/01application_program/FileManipulatorProgram/reverse.go
+++ backend_project/01application_program/FileManipulatorProgram/reverse.go
@@ -7,6 +7,9 @@ import (
 )
 
 func main() {
+	//"Usage: go run file_manipulator.go reverse <inputfile> <outputfile>"
+
+	//コマンドラインから引数を取得する。
 	if len(os.Args) < 4 {
 		fmt.Println("Usage: go run file_manipulator.go reverse <inputfile> <outputfile>")
 		return
@@ -16,6 +19,8 @@ func main() {
 	inputFile := os.Args[2]
 	outputFile := os.Args[3]
 
+	//反転させたいファイル（入力ファイル）を開く
+
 	if command == "reverse" {
 		file, err := os.Open(inputFile)
 		if err != nil {
@@ -23,14 +28,17 @@ func main() {
 			return
 		}
 		defer file.Close()
+		//入力ファイルから読み込みバイトスライスとして返す。
 
 		content, err := io.ReadAll(file)
 		if err != nil {
 			fmt.Println("Error reading content:", err)
 			return
 		}
-
+		//バイトスライスをstringに変換する
 		reversedData := reverseString(string(content))
+		//逆になった読み込みデータを出力ファイルを指定して書き込む。
+
 		err = os.WriteFile(outputFile, []byte(reversedData), 0644)
 		if err != nil {
 			fmt.Println("Error writing to file:", err)
@@ -38,6 +46,9 @@ func main() {
 		}
 	}
 }
+
+//文字列を逆に変換する関数を作成する
+//コードポイント単位(rune)で扱うとよい。文字列型、バイト型はUTF-8エンコードされた場においてUnicode文字はマルチバイトが多いので、不正な文字列が生成されやすい
 
 func reverseString(s string) string {
 	runes := []rune(s)
