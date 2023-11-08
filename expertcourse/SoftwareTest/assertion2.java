6a92204f71d5c69dc8222d1202f06f2be84aa53e
小泉孝雄 <takoizmi22@gmail.com>
2023-11-08 22:19:36 +0900

    1108

--- expertcourse/SoftwareTest/assertion2.java
+++ expertcourse/SoftwareTest/assertion2.java
@@ -8,13 +8,23 @@ class Assertion {
 
 class MyClass {
     public static int applyDiscount(int originalPrice, double discount) {
-        double discountedPrice = originalPrice * (1.0 - discount);
-        System.out.println((int) discountedPrice);
-
+        double discountedPrice; 
+        if (discount > 0 && discount < 1) {
+            discountedPrice = originalPrice * (1.0 - discount);
+        } else {
+            discountedPrice = originalPrice;
+        }
         return (int) discountedPrice;
     }
 
     public static void main(String[] args) throws Exception {
         // ここから書きましょう
+        Assertion.run(applyDiscount(30,0.5) == 15);
+        Assertion.run(applyDiscount(50,0.2) == 40);
+        Assertion.run(applyDiscount(70,1.0) == 70);
+        Assertion.run(applyDiscount(70,0.0) == 70);
+        Assertion.run(applyDiscount(90,2.0) == 90);
+        Assertion.run(applyDiscount(30,-3.0) == 30);
+
     }
 }
