00901fef39ec141d99085a4e241108632404e882
小泉孝雄 <takoizmi22@gmail.com>
2023-11-21 22:30:46 +0900

    1121

--- Database/advancedSQL/SalesManagementSystem.sql
+++ Database/advancedSQL/SalesManagementSystem.sql
@@ -1,3 +1,13 @@
 -- クエリを書いてください。 --
 -- https://recursionist.io/dashboard/course/6/lesson/1050
-SELECT member_id,COUNT(*) FROM negotiations WHERE status = 'SUCCESS' AND done_at LIKE '2021-10-%' GROUP BY member_id ORDER BY COUNT(*) DESC,member_id;
+SELECT
+ member_id,COUNT(*) 
+FROM
+ negotiations 
+WHERE 
+ status = 'SUCCESS' AND done_at LIKE '2021-10-%' 
+GROUP BY
+ member_id 
+ORDER BY 
+ COUNT(*) DESC,
+ member_id;
